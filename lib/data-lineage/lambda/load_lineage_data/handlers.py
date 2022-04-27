bdbdfbac6672aa563cea06a4d135cc555fff1588
Dean Ford <awsford@amazon.co.uk>
2022-04-27 14:47:02 -0700

    placeholder for put into DDB

--- lib/data-lineage/lambda/load_lineage_data/handlers.py
+++ lib/data-lineage/lambda/load_lineage_data/handlers.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import boto3
 
 from aws_lambda_powertools import Logger
@@ -13,5 +14,9 @@ class DBHandler:
         self.logger = Logger("hpc.dbhandler", child=True)
         self.db = session.resource("dynamodb")
         self.table = self.db.Table(table_name)
-    
+
+    def put(self, item: Dict) -> int:
+        self.logger.info(f"Adding item '{item}' to table {self.table.name}")
+        response = self.table.put_item(Item=item)
+        return response["ResponseMetadata"]["HTTPStatusCode"]
 
