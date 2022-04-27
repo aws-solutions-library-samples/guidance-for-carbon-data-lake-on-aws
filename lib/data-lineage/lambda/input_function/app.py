1244b9755f30216a8e0963d256cdfe80d4d7205d
Dean Ford <awsford@amazon.co.uk>
2022-04-27 12:05:14 -0700

    take record, send to queue - basic

--- lib/data-lineage/lambda/input_function/app.py
+++ lib/data-lineage/lambda/input_function/app.py
@@ -1,10 +1,15 @@
 import json
+import os
+import string
+import random
+import time
 from typing import Dict, List
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.tracing import Tracer
-from aws_lambda_powertools.utilities.data_classes import SQSEvent
+
+from handlers import DataHandler
 
 logger = Logger(service="carbonlake", level="debug")
 tracer = Tracer()
@@ -13,7 +18,9 @@ tracer = Tracer()
 class InputEvent:
   root_id: str
   parent_id: str
-  action: str
+  action_taken: str
+  storage_type: str
+  storage_location: str
 
 """
 INPUT: Event from the central SFN workflow with records to be added to the queue for data lineage ledger
@@ -24,4 +31,21 @@ OUTPUT: Return an array of child_id's for the next
 @tracer.capture_lambda_handler()
 def lambda_handler(event: Dict, context: Dict):
   event = InputEvent(**event)
+  data_handler = DataHandler(os.environ["SQS_QUEUE_URL"])
+
+  # generate uid - the child_id will become the parent_id for subsequent child nodes
+  alphabet = string.ascii_letters + string.digits
+  child_id = "".join(random.choices(alphabet, k=8))
+
+  # timestamp attribute is used to restructure the tree and set TTL on DDB
+  recordedAt = int(time.time())
+
+  message = {
+    **asdict(event),
+    "child_id": child_id,
+    "recordedAt": recordedAt
+  }
+
+  # publish record to queue
+  data_handler.sqs.send_message(json.dumps(message))
   return
