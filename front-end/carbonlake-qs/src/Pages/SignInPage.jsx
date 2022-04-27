cc1f98ff8c24cab91ef143da9ab8ea363b4dd626
Kevon Mayers <novekm@amazon.com>
2022-04-27 11:37:43 -0700

    Edit SignInPage.jsx. Edit WelcomeCard component

--- front-end/carbonlake-qs/src/Pages/SignInPage.jsx
+++ front-end/carbonlake-qs/src/Pages/SignInPage.jsx
@@ -1,6 +1,6 @@
 // import React from "react";
 // import Sidebar from './components/Sidebar'
-import SignIn from '../components/SignIn'
+import SignIn from '../components/Signin'
 
 const SignInPage = () => {
   return(
--- front-end/carbonlake-qs/src/components/MainContent/WelcomeCard/index.js
+++ front-end/carbonlake-qs/src/components/MainContent/WelcomeCard/index.js
@@ -1,13 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { MainContentContainer } from './MainContent.styles';
-import MUIButton from '@material-ui/core/Button'
-import MUIButtonGroup from '@material-ui/core/Button'
-import MUITextField from '@material-ui/core/TextField'
-
-import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-
-import { Storage } from 'aws-amplify';
 
 // Icons
 import SaveIcon from '@material-ui/icons/Save'
@@ -17,48 +9,44 @@ import DeleteIcon from '@material-ui/icons/Delete'
 // import {
 //   UserProfilePicture
 // } from '../Sidebar/UserProfile/UserProfile.styles';
-import UserProfilePic from '../../images/image1.jpg';
-import UserDefaultPicture from '../../images/default-user-picture.png'
-
-import WelcomeCard from './WelcomeCard';
-import {day,month,date,year} from './CurrentDate/DateFetch'
-import {CurrentDateContainer,CurrentDate} from './CurrentDate/DateFetch.styles'
-
-import GridInfoCardGroup from './GridInfoCardGroup'
-
-import JobList from './JobList'
-import { JobListContainer } from './JobList/JobList.styles';
-
-import TranscribeJobList from './TranscribeJobList';
+// import WelcomeCardImage from '../../../images/undraw_wind_turbine_green.svg';
+import WelcomeCardImage from '../../../images/undraw_working_remotely_green.svg';
+import {
+  WelcomeCardContainer,
+  WelcomeCardGrid,
+  WelcomeCardTextContainer,
+  WelcomeCardTitle,
+  WelcomeCardSubtitle,
+  WelcomeCardEmoji,
+  WelcomeCardImageContainer,
+} from './WelcomeCard.styles'
 
 
-const MainContent = () => {
+
+const WelcomeCard = () => {
   return (
 <>
 
-<MainContentContainer>
-    {/* <CurrentDateContainer>
-        <CurrentDate>
-            {month} {date}, {year}
-        </CurrentDate>
-    </CurrentDateContainer>
+<WelcomeCardContainer>
+  <WelcomeCardGrid>
 
-    <WelcomeCard />
-    <GridInfoCardGroup />
-    <JobListContainer>
+  <WelcomeCardTextContainer>
+    <WelcomeCardTitle>
+    Hi, Kevon
+    <WelcomeCardEmoji role="img" aria-label="dog">
+    👋
+    </WelcomeCardEmoji>
+    </WelcomeCardTitle>
+    <WelcomeCardSubtitle>
+    Ready to get to work?
+    </WelcomeCardSubtitle>
+  </WelcomeCardTextContainer>
+    <WelcomeCardImageContainer src = {WelcomeCardImage}/>
+  </WelcomeCardGrid>
+</WelcomeCardContainer>
 
-    <JobList />
-    </JobListContainer>
-    <JobListContainer>
-
-    <TranscribeJobList />
-    </JobListContainer>
- */}
-
-
-</MainContentContainer>
 </>
   )
 }
 
-export default MainContent;
+export default WelcomeCard;
