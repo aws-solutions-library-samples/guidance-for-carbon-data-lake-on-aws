0b2d660f4d8813e6171d534a40b3f2f79b88468d
Dean Ford <awsford@amazon.co.uk>
2022-04-27 12:17:58 -0700

    can successfully receive message from sqs

--- lib/data-lineage/lambda/load_lineage_data/app.py
+++ lib/data-lineage/lambda/load_lineage_data/app.py
@@ -5,6 +5,8 @@ from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.tracing import Tracer
 from aws_lambda_powertools.utilities.data_classes import SQSEvent
 
+from handlers import DataHandler
+
 logger = Logger(service="carbonlake", level="debug")
 tracer = Tracer()
 
@@ -13,8 +15,14 @@ INPUT: Event from SQS with records to be added to the data lineage ledger
 PROCESS: For each input record, fetch additional metadata from CarbonLake and store in DDB
 OUTPUT: None
 """
+
+
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler()
 def lambda_handler(event: Dict, context: Dict):
-  event = SQSEvent(event)
-  return
+    event = SQSEvent(event)
+    for record in event.records:
+        message = json.loads(record.body)
+        logger.info(f"Processing record - child id = {message['child_id']}")
+
+    return
---
+++ lib/data-lineage/lambda/load_lineage_data/handlers.py
@@ -0,0 +1,17 @@
+import boto3
+
+from aws_lambda_powertools import Logger
+
+
+class DataHandler:
+    def __init__(self, table_name) -> None:        
+        self.session = boto3.Session()
+        self.db = DBHandler(self.session, table_name)
+
+class DBHandler:
+    def __init__(self, session, table_name) -> None:
+        self.logger = Logger("hpc.dbhandler", child=True)
+        self.db = session.resource("dynamodb")
+        self.table = self.db.Table(table_name)
+    
+
