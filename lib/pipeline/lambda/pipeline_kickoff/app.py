13895194d7e0179eb039faa14ffb2760aab4c6f0
Dean Ford <awsford@amazon.co.uk>
2022-04-28 08:57:24 -0700

    move function out of statemachine execution

--- lib/pipeline/lambda/pipeline_kickoff/app.py
+++ lib/pipeline/lambda/pipeline_kickoff/app.py
@@ -10,15 +10,17 @@ logger = Logger(service="carbonlake", level="debug")
 tracer = Tracer()
 
 """
-INPUT: Blank SFN initiation event
+INPUT: S3 PUT Event from the Landing S3 bucket
 PROCESS: 
     1. generate the initial hash id for the job (this will be the DL root_id for all child records)
     2. determine the file type of the input to choose the correct process / DQ workflow
-OUTPUT: Return an array of child_id's for the next 
+    3. start the step function to orchestrate to CO2e calculation job workflow
+OUTPUT: None
 """
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler()
 def lambda_handler(event: Dict, context: Dict):
+
     # DATA LINEAGE
     data_lineage = {}
     ## alphabet used for generating uids randomly
@@ -35,4 +37,9 @@ def lambda_handler(event: Dict, context: Dict):
         "storage_type": "s3",
         "storage_location": "s3://<landing_bucket>/csv/raw.csv" # will get this from input event
     }
-    return json.dumps({ "file": file_attributes, "data_lineage": data_lineage })
+
+    
+    # START STEP FUNCTION EXECUTION
+    sfn_payload = { "file": file_attributes, "data_lineage": data_lineage } 
+    
+    return json.dumps(sfn_payload)
