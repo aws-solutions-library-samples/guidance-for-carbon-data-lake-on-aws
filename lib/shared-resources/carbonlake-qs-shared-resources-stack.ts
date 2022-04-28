4783b0023dcb0cc4b15941991433e518ad8d8138
Sam Biddle <sambiddl@amazon.com>
2022-04-28 21:48:11 +0000

    Resolve "QS9.0-carbonlake-main-stack"

---
+++ lib/shared-resources/carbonlake-qs-shared-resources-stack.ts
@@ -0,0 +1,24 @@
+import { App, Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';                 // core constructs
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+
+export class CarbonlakeQuickstartSharedResourcesStack extends Stack {
+    constructor(scope: App, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        // const carbonlakeProcessedDataBucket = new s3.Bucket(this, 'carbonlakeProcessedDataBucket', {
+        //     removalPolicy: RemovalPolicy.DESTROY,
+        //   });
+        
+        // const carbonlakeCuratedDataBucket = new s3.Bucket(this, 'carbonlakeCuratedDataBucket', {
+        //     removalPolicy: RemovalPolicy.DESTROY,
+        //   });
+    
+        // // Output bucket reference for nested stacks
+        // new CfnOutput(this, 'myBucketRef', {
+        // value: carbonlakeProcessedDataBucket.bucketName,
+        // description: 'The name of the processed data s3 bucket',
+        // exportName: 'carbonlakeProcessedDataBucket',
+        // });
+    
+    }
+}
