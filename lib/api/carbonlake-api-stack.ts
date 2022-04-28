65829a05ad233cfd3923a1a717b41208ca1fdcac
Jon Terry <jkterry@amazon.com>
2022-04-28 16:19:16 -0700

    Add Cognito user pool to api

--- lib/api/carbonlake-api-stack.ts
+++ lib/api/carbonlake-api-stack.ts
@@ -2,8 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as path from 'path';
 import { AuthorizationType, FieldLogLevel, GraphqlApi, MappingTemplate, Schema } from '@aws-cdk/aws-appsync-alpha';
-import { CfnLoggingConfiguration } from 'aws-cdk-lib/aws-networkfirewall';
 import { CfnOutput } from 'aws-cdk-lib';
+import { IUserPool, IUserPoolClient, UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 
 export interface CarbonLakeQuickStartApiStackProps extends cdk.StackProps {
     calculatorOutputTableRef: cdk.aws_dynamodb.Table;
@@ -11,9 +11,30 @@ export interface CarbonLakeQuickStartApiStackProps extends cdk.StackProps {
 
 export class CarbonLakeQuickStartApiStack extends cdk.Stack {
     public readonly graphqlUrl: string;
+    public readonly apiId: string;
+    public readonly userPool: IUserPool;
+    public readonly userPoolClient: IUserPoolClient;
 
     constructor(scope: Construct, id: string, props: CarbonLakeQuickStartApiStackProps) {
         super(scope, id, props);
+
+        // Create a sample Cognito user pool to use in providing authentication & authorization for the API
+        const userPool = new UserPool(this, 'CarbonLakeQuickStartUserPool', {
+            userPoolName: 'CarbonLakeQuickStartUserPool'
+        });
+
+        // Create a sample Cognito user pool client to allow users created in Cognito to login and use the API
+        const userPoolClient = new UserPoolClient(this, 'CarbonLakeQuickStartUserPoolClient', {
+            userPool: userPool,
+            userPoolClientName: 'CarbonLakeQuickStartUserPoolClient',
+            generateSecret: false
+        });
+
+        // Set the public variables so other stacks can access the deployed userPoolId & userPoolClientId as well as set as CloudFormation output variables
+        this.userPool = userPool;
+        new CfnOutput(this, 'userPoolId', {value: userPool.userPoolId});
+        this.userPoolClient = userPoolClient;
+        new CfnOutput(this, 'userPoolClientId', {value: userPoolClient.userPoolClientId});
 
         // Create the GraphQL api and provide the schema.graphql file
         const api = new GraphqlApi(this, 'CarbonLakeApi', {
@@ -21,7 +42,10 @@ export class CarbonLakeQuickStartApiStack extends cdk.Stack {
             schema: Schema.fromAsset(path.join(__dirname, 'schema.graphql')),
             authorizationConfig: {
                 defaultAuthorization: {
-                    authorizationType: AuthorizationType.API_KEY
+                    authorizationType: AuthorizationType.USER_POOL,
+                    userPoolConfig: {
+                        userPool: userPool
+                    }
                 },
             },
             logConfig: {
@@ -32,9 +56,11 @@ export class CarbonLakeQuickStartApiStack extends cdk.Stack {
             //xrayEnabled: true
         });
 
-        // Set the public variable so other stacks can access the deployed graphqlUrl and set as a CloudFormation output variable
+        // Set the public variables so other stacks can access the deployed graphqlUrl & apiId as well as set as CloudFormation output variables
         this.graphqlUrl = api.graphqlUrl;
-        new CfnOutput(this, 'apiUrl', {value: api.graphqlUrl});
+        new CfnOutput(this, 'graphqlUrl', {value: api.graphqlUrl});
+        this.apiId = api.apiId;
+        new CfnOutput(this, 'apiId', {value: api.apiId});
 
         // Add a DynamoDB datasource. The DynamoDB table we will use is created by another stack
         // and is provided in the props of this stack.
