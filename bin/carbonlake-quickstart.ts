5a160b3bd545c399fa1a37fc83034c193261d7f6
Sam Biddle <sambiddl@amazon.com>
2022-05-12 13:17:34 -0400

    Remove package-lock.json from .gitignore file.

--- bin/carbonlake-quickstart.ts
+++ bin/carbonlake-quickstart.ts
@@ -2,19 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CarbonlakeQuickstartStack } from '../lib/carbonlake-qs-stack';
+import { CarbonlakeQuickstartCiCdPipelineStack } from '../lib/ci-cd/carbonlake-qs-ci-cd-pipeline-stack';
 
 const app = new cdk.App();
+new CarbonlakeQuickstartCiCdPipelineStack(app, 'CarbonLakeQuickstartCiCdPipelineStack');
+
+/*
 new CarbonlakeQuickstartStack(app, 'CarbonlakeQuickstartStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+*/
