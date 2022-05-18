a4a931993c3cb84e92c8d7b0fbcb0387be92b384
Kevon Mayers <novekm@amazon.com>
2022-05-17 23:05:46 -0400

    Remove use of HashRouter for app

--- front-end/carbonlake-ui/src/index.jsx
+++ front-end/carbonlake-ui/src/index.jsx
@@ -4,8 +4,11 @@ import { HashRouter } from "react-router-dom";
 import App from "./pages/App";
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  // <HashRouter>
+  //   <App />
+  // </HashRouter>,
+  <React.StrictMode>
+  <App />
+</React.StrictMode>,
   document.getElementById("root")
 );
