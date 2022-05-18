c468d6ba6df78d10a093be504d5bd7403e6fa5c9
Kevon Mayers <novekm@amazon.com>
2022-05-17 23:07:43 -0400

    Use react-router-dom v6, import amplify auth

--- front-end/carbonlake-ui/src/Pages/App.jsx
+++ front-end/carbonlake-ui/src/Pages/App.jsx
@@ -8,7 +8,7 @@ guidelines:
 https://polaris.corp.amazon.com/getting_started/development/integration/
 ************************************************************************/
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import BobRossIntro from './BobRossIntro.jsx';
 import Dashboard from './Dashboard.jsx';
 import CarbonLake101 from './CarbonLake101.jsx';
@@ -16,55 +16,62 @@ import SetupGuide from './SetupGuide.jsx';
 import DataUploader from './DataUploader.jsx';
 import AccountSettings from './AccountSettings.jsx';
 // import Visualizations from './Visualizations.jsx';
-import Basic from './BasicLayout.jsx';
-import ServiceHomepage from './ServiceHomepage.jsx';
-import CreateForm from './CreateForm.jsx';
+// import Basic from './BasicLayout.jsx';
+// import ServiceHomepage from './ServiceHomepage.jsx';
+// import CreateForm from './CreateForm.jsx';
 import TableView from './EmissionRecords.jsx';
-import EmptyTableView from './TableWithEmptyState.jsx';
+// import EmptyTableView from './TableWithEmptyState.jsx';
 
 import '@awsui/global-styles/index.css';
 
-
 // Amplify
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-import { AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react'
+import Amplify, { Auth, Storage, API, graphqlOperation } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
 
 
-Amplify.configure(awsconfig);
 // Class App is the "output" generated on every build,
 // it is what you will see on the webpage.
-function App() {
+function App({signOut, user}) {
     return (
       // When you create a new file or template, add it below
       // as a new 'Route' so you can link to it with a url.
 
       <div>
-        {/* <Route exact path="/" component={BobRossIntro} /> */}
-        {/* <Route exact path="/" component={CarbonLake101} /> */}
-        {/* <Route exact path="/" component={Dashboard} /> */}
-        <Route  path="/carbonlake-101" component={CarbonLake101} />
-        <Route  path="/setup-guide" component={SetupGuide} />
-        <Route  path="/data-uploader" component={DataUploader} />
-        <Route  path="/account-settings" component={AccountSettings} />
-        {/* <Route exact path="/visualizations" component={Visualizations} /> */}
-        <Route path="/basic" component={Basic} />
-        <Route path="/service-home" component={ServiceHomepage} />
-        <Route path="/create" component={CreateForm} />
-        <Route path="/emission-records" component={TableView} />
-        <Route path="/table-empty" component={EmptyTableView} />
-        <Route path='/about-carbonlake' component={() => {
-            window.location.href = 'https://aws.amazon.com';
-          return null;
-        }}/>
-        <Route path='/submit-issue' component={() => {
-            window.location.href = 'https://github.com';
-          return null;
-        }}/>
+        <Router>
 
-        <AmplifySignOut />
+        <Routes>
+
+        {/* <Route exact path="/" element={BobRossIntro} /> */}
+        <Route  path="/" element={<CarbonLake101 />} />
+        {/* <Route exact path="/" element={Dashboard} /> */}
+        <Route  path="/carbonlake-101" element={<CarbonLake101 />} />
+        <Route  path="/setup-guide" element={<SetupGuide />} />
+        <Route  path="/data-uploader" element={<DataUploader />} />
+        <Route  path="/account-settings" element={<AccountSettings />} />
+        {/* <Route exact path="/visualizations" element={Visualizations} /> */}
+        {/* <Route path="/basic" element={<Basic />} />
+        <Route path="/service-home" element={<ServiceHomepage />} />
+      <Route path="/create" element={<CreateForm />} /> */}
+        <Route path="/emission-records" element={<TableView />} />
+        {/* <Route path="/table-empty" element={<EmptyTableView />} /> */}
+        <Route path='/about-carbonlake' element={() => {
+          window.location.href = 'https://aws.amazon.com';
+          return null;
+        }}/>
+        <Route path='/submit-issue' element={() => {
+          window.location.href = 'https://github.com';
+          return null;
+        }}/>
+        </Routes>
+        </Router>
+
       </div>
     );
-
 }
+
 export default withAuthenticator(App);
