deac54ba37ec95933426c433c29bbfee340ae9b0
Kevon Mayers <novekm@amazon.com>
2022-05-17 23:05:20 -0400

    update vite.config.js-fix uncaught referenceerror

--- front-end/carbonlake-ui/vite.config.js
+++ front-end/carbonlake-ui/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "",
+//   plugins: [react()]
+// });
+
 export default defineConfig({
-  base: "",
-  plugins: [react()]
+  define: {
+    "global": {},
+  },
 });
