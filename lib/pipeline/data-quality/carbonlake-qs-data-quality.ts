4c0d8b6ea70d4ded29d2104652946be53987afe3
Dean Ford <awsford@amazon.co.uk>
2022-06-22 13:34:41 +0100

    boilerplate for integration to pipeline

--- lib/pipeline/data-quality/carbonlake-qs-data-quality.ts
+++ lib/pipeline/data-quality/carbonlake-qs-data-quality.ts
@@ -1,10 +1,35 @@
-// This is a placeholder for the glue data quality stack
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
-import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
-export class DataQualityStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+interface DataQualityStackProps extends StackProps {
+  inputBucket: s3.Bucket;
+  outputBucket: s3.Bucket;
+}
+
+/*
+  Databrew jobs require a static dataset location at the point of creation
+  so it is not possible to create the databrew resources within CDK, as the
+  pipleine is expecting to provide an s3 key to be processed. Instead, this
+  stack defines a lambda function to handle the deployment and tear down of
+  databrew resources to be executed from within the step functions pipeline.
+*/
+export class DataQualityStack extends Stack {
+  constructor(scope: Construct, id: string, props: DataQualityStackProps) {
     super(scope, id, props);
+
+    /* ====== PERMISSIONS ====== */
+
+    // role used by the databrew profiling job -> read/write to S3
+
+    /* ====== RESOURCES LAMBDA ====== */
+
+    // lambda function to handle setup and teardown of databrew resources
+
+    /* ====== CHECK RESULTS LAMBDA ====== */
+
+    // lambda function to check the results of the databrew profiling job
+
   }
-  
 }
