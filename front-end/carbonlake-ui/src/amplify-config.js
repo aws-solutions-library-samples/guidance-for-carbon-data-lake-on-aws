67eeb0b6b073962bc6d8f8845488d7697e255a4e
Kevon Mayers <novekm@amazon.com>
2022-07-01 14:30:19 -0400

    Update amplify-config.js

--- front-end/carbonlake-ui/src/amplify-config.js
+++ front-end/carbonlake-ui/src/amplify-config.js
@@ -4,8 +4,8 @@
 const existingAPI = {
   // ...
   API: {
-    aws_appsync_graphqlEndpoint: 'https://gckuq7n4yrh45hx5zvgp2ecxua.appsync-api.us-east-1.amazonaws.com/graphql', // Replace with your GraphQL Endpoint
-    aws_appsync_region: 'us-east-1', // Replace with the region you deployed CDK with
+    aws_appsync_graphqlEndpoint: 'https://abcedfg123456.appsync-api.YOUR-AWS-REGION.amazonaws.com/graphql', // Replace with your GraphQL Endpoint
+    aws_appsync_region: 'YOUR-AWS-REGION', // Replace with the region you deployed CDK with
     aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS', // No touchy
   }
 
@@ -19,50 +19,22 @@ const existingAuth = {
     identityPoolId: 'us-east-1:52468e33-f23a-4ae2-92ca-071bd1bcb366',
 
     // REQUIRED - Amazon Cognito Region
-    region: 'us-east-1',
+    region: 'YOUR-AWS-REGION', // Replace with the region you deployed CDK with
 
     // OPTIONAL - Amazon Cognito Federated Identity Pool Region
     // Required only if it's different from Amazon Cognito Region
-    identityPoolRegion: 'us-east-1',
+    identityPoolRegion: 'YOUR-AWS-REGION',
 
-    // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: 'us-east-1_5PG9MH9sj',
+    // REQUIRED - Amazon Cognito User Pool ID
+    userPoolId: 'YOUR-AWS-REGOIN_abc123', // Replace with your User Pool ID
 
-    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: '74or209ii70dhrkusnq5pt0sd7',
+    // REQUIRED - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: 'abcdefg123456', // Replace with your User Pool Web Client ID
 
     // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
     mandatorySignIn: false,
 
-    // OPTIONAL - Configuration for cookie storage
-
-
-    // // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-    // cookieStorage: {
-    // // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-    //     domain: '.yourdomain.com',
-    // // OPTIONAL - Cookie path
-    //     path: '/',
-    // // OPTIONAL - Cookie expiration in days
-    //     expires: 365,
-    // // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-    //     sameSite: "strict" | "lax",
-    // // OPTIONAL - Cookie secure flag
-    // // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-    //     secure: true
-    // },
-
-
-    // OPTIONAL - customized storage object
-    // storage: MyStorage,
-
-    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-    // authenticationFlowType: 'USER_PASSWORD_AUTH',
-
-    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-    clientMetadata: { myCustomKey: 'myCustomValue' },
-
-     // OPTIONAL - Hosted UI configuration
+    // OPTIONAL - Hosted UI configuration
     oauth: {
         domain: 'your_cognito_domain',
         scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
@@ -70,8 +42,7 @@ const existingAuth = {
         redirectSignOut: 'http://localhost:3000/',
         responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
     }
-}
-
+  }
 }
 
 // Amplify.configure(existingAuth);
