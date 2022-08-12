c259fa1d1b8cab28d51464fe0e0ac1fc81f8a52d
Sam Biddle <sambiddl@amazon.com>
2022-08-12 01:03:39 -0400

    Merge changes and update new files.

--- lib/stacks/stack-shared-resources/carbonlake-qs-shared-resources-stack.ts
+++ lib/stacks/stack-shared-resources/carbonlake-qs-shared-resources-stack.ts
@@ -1,84 +1,99 @@
-import { App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib'
-import { aws_s3 as s3 } from 'aws-cdk-lib'
-import { aws_glue as glue } from 'aws-cdk-lib'
-import { Construct } from 'constructs'
+import { App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib'; 
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { aws_glue as glue } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class CarbonlakeQuickstartSharedResourcesStack extends Stack {
-  public readonly carbonlakeLandingBucket: s3.Bucket
-  public readonly carbonlakeRawBucket: s3.Bucket
-  public readonly carbonlakeErrorBucket: s3.Bucket
-  public readonly carbonlakeTransformedBucket: s3.Bucket
-  public readonly carbonlakeEnrichedBucket: s3.Bucket
-  public readonly carbonlakeForecastBucket: s3.Bucket
-  public readonly carbonlakeDataLineageBucket: s3.Bucket
-  public readonly glueEnrichedDataDatabase: glue.CfnDatabase
+    public readonly carbonlakeLandingBucket: s3.Bucket;
+    public readonly carbonlakeRawBucket: s3.Bucket;
+    public readonly carbonlakeErrorBucket: s3.Bucket;
+    public readonly carbonlakeTransformedBucket: s3.Bucket;
+    public readonly carbonlakeEnrichedBucket: s3.Bucket;
+    public readonly carbonlakeForecastBucket: s3.Bucket;
+    public readonly carbonlakeDataLineageBucket: s3.Bucket;
+    public readonly glueEnrichedDataDatabase: glue.CfnDatabase;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props)
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // Landing bucket where files are dropped by customers
-    // Once processed, the files are removed by the pipeline
-    this.carbonlakeLandingBucket = new s3.Bucket(this, 'carbonlakeLandingBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Landing bucket where files are dropped by customers
+        // Once processed, the files are removed by the pipeline
+        this.carbonlakeLandingBucket = new s3.Bucket(this, 'carbonlakeLandingBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            cors: [
+                    {
+                    allowedMethods: [
+                        s3.HttpMethods.GET,
+                        s3.HttpMethods.POST,
+                        s3.HttpMethods.PUT,
+                        s3.HttpMethods.DELETE,
+                        s3.HttpMethods.HEAD
+                    ],
+                    exposedHeaders: ['x-amz-server-side-encryption', 'x-amz-request-id', 'x-amz-id-2', 'ETag'],
+                    maxAge: 3000,
+                    allowedOrigins: ['*'],
+                    allowedHeaders: ['*'],
+                    },
+                ],
+        });
 
-    // Raw bucket where files are moved, once they pass the data quality check
-    // TODO add a lifecycle policy to archive files to Glacier
-    // TODO add a default lock on the objects (WORM)
-    this.carbonlakeRawBucket = new s3.Bucket(this, 'carbonlakeRawBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Raw bucket where files are moved, once they pass the data quality check
+        // TODO add a lifecycle policy to archive files to Glacier
+        // TODO add a default lock on the objects (WORM)
+        this.carbonlakeRawBucket = new s3.Bucket(this, 'carbonlakeRawBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // Error bucket where files are moved if they don't pass the data quality check
-    // Once manually processed, the files are manually removed from the bucket
-    this.carbonlakeErrorBucket = new s3.Bucket(this, 'carbonlakeErrorBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Error bucket where files are moved if they don't pass the data quality check
+        // Once manually processed, the files are manually removed from the bucket
+        this.carbonlakeErrorBucket = new s3.Bucket(this, 'carbonlakeErrorBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // Transformed bucket where files are moved after they are processed (format = jsonl)
-    // TODO add a lifecycle policy to archive files to Glacier
-    // TODO add a default lock on the objects (WORM)
-    this.carbonlakeTransformedBucket = new s3.Bucket(this, 'carbonlakeTransformedBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Transformed bucket where files are moved after they are processed (format = jsonl)
+        // TODO add a lifecycle policy to archive files to Glacier
+        // TODO add a default lock on the objects (WORM)
+        this.carbonlakeTransformedBucket = new s3.Bucket(this, 'carbonlakeTransformedBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // Enriched bucket where files are moved after they are enriched with calculated emissions (format = jsonl)
-    // A compacting job compacts jsonl files into parquet files every day
-    this.carbonlakeEnrichedBucket = new s3.Bucket(this, 'carbonlakeEnrichedBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Enriched bucket where files are moved after they are enriched with calculated emissions (format = jsonl)
+        // A compacting job compacts jsonl files into parquet files every day
+        this.carbonlakeEnrichedBucket = new s3.Bucket(this, 'carbonlakeEnrichedBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // Used for forecasting
-    this.carbonlakeForecastBucket = new s3.Bucket(this, 'carbonlakeForecastBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Used for forecasting
+        this.carbonlakeForecastBucket = new s3.Bucket(this, 'carbonlakeForecastBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // Bucket used to store unprocessed data lineage events
-    // TODO add a lifecycle policy to archive files to Glacier
-    this.carbonlakeDataLineageBucket = new s3.Bucket(this, 'carbonlakeDataLineageBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
+        // Bucket used to store unprocessed data lineage events
+        // TODO add a lifecycle policy to archive files to Glacier
+        this.carbonlakeDataLineageBucket = new s3.Bucket(this, 'carbonlakeDataLineageBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // Glue Metadata Catalog Database for enriched data
-    this.glueEnrichedDataDatabase = new glue.CfnDatabase(this, 'enrichedCalculatorDataDatabase', {
-      catalogId: this.account,
-      databaseInput: {
-        description: 'Glue Metadata Catalog  database for enriched calculator data',
-      },
-    })
-  }
+        // Glue Metadata Catalog Database for enriched data
+        this.glueEnrichedDataDatabase = new glue.CfnDatabase(this, "enrichedCalculatorDataDatabase", {
+            catalogId: this.account,
+            databaseInput: {
+              description: 'Glue Metadata Catalog  database for enriched calculator data',
+            },
+          });
+    }
 }
