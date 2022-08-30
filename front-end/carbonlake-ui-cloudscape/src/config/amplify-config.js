04125bd74770f78c51e42407ef19cbcaff3d35f1
Kevon Mayers <novekm@amazon.com>
2022-08-30 15:38:16 -0400

    update amplify-config

--- front-end/carbonlake-ui-cloudscape/src/config/amplify-config.js
+++ front-end/carbonlake-ui-cloudscape/src/config/amplify-config.js
@@ -1,61 +1,70 @@
+// TODO - Create cdk output for the user's current region so they don't
+// have to past in the region for their resources below
+
 // -- AWS AMPLIFY CONFIGURATION PARAMETERS --
+// To set up your own amplify app you will need to rename this file as amplify-config.js and follow the instructions in the README to fill in your values
+import cdkOutputsJSON from '../../../../cdk-outputs.json'
 
-import outputsJSON from '../../../terraform-deployment/modules/tca-qs/outputs.json'
-import { Amplify, Auth, Storage} from 'aws-amplify';
 
+// -- AWS AMPLIFY CONFIGURATION PARAMETERS --
 const AmplifyConfig = {
 
-      // Existing API
-      API: {
-        aws_appsync_graphqlEndpoint: outputsJSON.outputs.tca_appsync_graphql_api_uris.value.GRAPHQL, // Replace with your GraphQL Endpoint
-        aws_appsync_region: outputsJSON.outputs.tca_aws_current_region.value, // Replace with the region you deployed CDK with
-        aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS', // No touchy
-      },
+// Existing API
+   API: {
+    aws_appsync_graphqlEndpoint:
+    cdkOutputsJSON.ApiStack.graphqlUrl, // Replace with your GraphQL Endpoint
+    aws_appsync_region: "us-east-1", // Replace with the region you deployed CDK with
+    // aws_appsync_region: cdkOutputsJSON.SharedResources.currentRegion, // Todo - create output for currentRegion
+    aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS", // No touchy
+  },
 
+  // Existing Auth
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: cdkOutputsJSON.ApiStack.identityPoolId,
 
-    // Existing Auth
-      Auth: {
+    // REQUIRED - Amazon Cognito Region
+    region: "us-east-1", // Replace with the region you deployed CDK with
+    // region: cdkOutputsJSON.SharedResources.currentRegion, // Replace with the region you deployed CDK with
 
-        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-        identityPoolId: outputsJSON.outputs.tca_identity_pool_id.value,
+    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+    // Required only if it's different from Amazon Cognito Region
+    identityPoolRegion: "us-east-1",
+    // identityPoolRegion: cdkOutputsJSON.SharedResources.currentRegion,
 
-        // REQUIRED - Amazon Cognito Region
-        region: outputsJSON.outputs.tca_aws_current_region.value, // Replace with the region you deployed CDK with
+    // REQUIRED - Amazon Cognito User Pool ID
+    userPoolId: cdkOutputsJSON.ApiStack.userPoolId, // Replace with your User Pool ID
 
-        // OPTIONAL - Amazon Cognito Federated Identity Pool Region
-        // Required only if it's different from Amazon Cognito Region
-        identityPoolRegion: outputsJSON.outputs.tca_aws_current_region.value,
+    // REQUIRED - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: cdkOutputsJSON.ApiStack.userPoolClientId, // Replace with your User Pool Web Client ID
 
-        // REQUIRED - Amazon Cognito User Pool ID
-        userPoolId: outputsJSON.outputs.tca_user_pool_id.value, // Replace with your User Pool ID
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: false,
 
-        // REQUIRED - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: outputsJSON.outputs.tca_user_pool_client_id.value, // Replace with your User Pool Web Client ID
+    // OPTIONAL - Hosted UI configuration
+    oauth: {
+      domain: "your_cognito_domain",
+      scope: [
+        "phone",
+        "email",
+        "profile",
+        "openid",
+        "aws.cognito.signin.user.admin",
+      ],
+      redirectSignIn: "http://localhost:3000/",
+      redirectSignOut: "http://localhost:3000/",
+      responseType: "code", // or 'token', note that REFRESH token will only be generated when the responseType is code
+    },
+  },
 
-        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-        mandatorySignIn: false,
-
-        // OPTIONAL - Hosted UI configuration
-        oauth: {
-            domain: 'your_cognito_domain',
-            scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-            redirectSignIn: 'http://localhost:3000/',
-            redirectSignOut: 'http://localhost:3000/',
-            responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
-        }
-      },
-
-
-
-      Storage: {
-            AWSS3: {
-                bucket: outputsJSON.outputs.tca_landing_bucket.value, //REQUIRED -  Amazon S3 bucket name
-                region: outputsJSON.outputs.tca_aws_current_region.value, //Required -  Amazon service region
-            },
-        }
-
-
-
+  // Existing Storage
+  Storage: {
+    AWSS3: {
+      bucket: cdkOutputsJSON.DataPipelineStack.LandingBucketName, //REQUIRED -  Amazon S3 bucket name
+      region: "us-east-1", //Required -  Amazon service region
+      // region: cdkOutputsJSON.SharedResources.currentRegion, //Required -  Amazon service region
+    },
+  },
 
 }
 
