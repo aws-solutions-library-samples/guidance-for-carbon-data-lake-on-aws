e5a7df540f0ba7be702fa8892d98752e49f3140f
Sam Biddle <sambiddl@amazon.com>
2022-08-31 11:42:04 -0400

    Revert "Add vite options."
    This reverts commit 23854190f961bc055a745eafaf6ca4b6ecf9819c.

--- front-end/carbonlake-ui-cloudscape/vite.config.js
+++ front-end/carbonlake-ui-cloudscape/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "",
+//   plugins: [react()]
+// });
 
 export default defineConfig({
-build: {
-  rollupOptions: {
-    external: [
-      "react", // ignore react stuff
-      "react-dom",
-      "/^@cloudscape.*/"
-    ],
+  define: {
+    "global": {},
   },
-},
 });
