23854190f961bc055a745eafaf6ca4b6ecf9819c
Sam Biddle <sambiddl@amazon.com>
2022-08-30 22:56:25 -0400

    Add vite options.

--- front-end/carbonlake-ui-cloudscape/vite.config.js
+++ front-end/carbonlake-ui-cloudscape/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "",
-//   plugins: [react()]
-// });
 
 export default defineConfig({
-  define: {
-    "global": {},
+build: {
+  rollupOptions: {
+    external: [
+      "react", // ignore react stuff
+      "react-dom",
+      "/^@cloudscape.*/"
+    ],
   },
+},
 });
