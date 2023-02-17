483df6339b1099aa83bc0568d6d91cd554a63e3e
Sam Biddle <sambiddl@amazon.com>
2023-02-17 13:54:01 +0000

    Enhancement/move all build commands to shell as variables/sambiddl

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # synthesize cdk with context inputs
-cdk synth --context --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"
 cdk bootstrap --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"  #bootstraps cdk in the region
+cdk synth --context --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"
 wait
 echo "🚀 deploying all"
 cdk deploy --all --context --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"
