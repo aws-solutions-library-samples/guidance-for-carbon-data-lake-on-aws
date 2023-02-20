129fa8140f97fc5008cf799f0dd1372389650fa4
Chris Gabriel <cmgabri@amazon.com>
2023-02-20 14:43:49 -0600

    Revert "feat:iot-data-ingestion"
    This reverts commit 218cf5d4e3ea9b256d07302959db50848c10d067.

--- lib/stacks/stack-iot-ingest/iot-ingest.ts
+++ lib/stacks/stack-iot-ingest/iot-ingest.ts
@@ -47,3 +47,4 @@ export class IotIngestStack extends Stack {
 }
 
 
+
--- test-deploy.sh
+++ test-deploy.sh
@@ -2,7 +2,7 @@
 
 # synthesize cdk with context inputs
 cdk bootstrap --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"  #bootstraps cdk in the region
-cdk synth --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"
+cdk synth --context --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"
 wait
 echo "🚀 deploying all"
 cdk deploy --all --context adminEmail="test@test.com" --context quicksightUsername="test@test.com" --context framework="ghg_protocol"
