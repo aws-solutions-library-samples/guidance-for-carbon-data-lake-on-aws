927cb6e106eef8415055ba4bea4adcde76081cc6
Sam Biddle <sambiddl@amazon.com>
2023-03-15 09:47:15 -0400

    Update shared resource buckets to CDL bucket.

--- lib/stacks/stack-shared-resources/stack-shared-resources.ts
+++ lib/stacks/stack-shared-resources/stack-shared-resources.ts
@@ -1,16 +1,17 @@
 import { Stack, StackProps, RemovalPolicy, PhysicalName, CfnOutput, Tags } from 'aws-cdk-lib'
 import { aws_s3 as s3 } from 'aws-cdk-lib'
+import { CdlS3 } from '../../constructs/construct-cdl-s3-bucket/construct-cdl-s3-bucket'
 import { aws_glue as glue } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 export class SharedResourcesStack extends Stack {
-  public readonly cdlLandingBucket: s3.Bucket
-  public readonly cdlRawBucket: s3.Bucket
-  public readonly cdlErrorBucket: s3.Bucket
-  public readonly cdlTransformedBucket: s3.Bucket
-  public readonly cdlEnrichedBucket: s3.Bucket
-  public readonly cdlForecastBucket: s3.Bucket
-  public readonly cdlDataLineageBucket: s3.Bucket
+  public readonly cdlLandingBucket: CdlS3
+  public readonly cdlRawBucket: CdlS3
+  public readonly cdlErrorBucket: CdlS3
+  public readonly cdlTransformedBucket: CdlS3
+  public readonly cdlEnrichedBucket: CdlS3
+  public readonly cdlForecastBucket: CdlS3
+  public readonly cdlDataLineageBucket: CdlS3
   public readonly glueEnrichedDataDatabase: glue.CfnDatabase
 
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -19,48 +20,33 @@ export class SharedResourcesStack extends Stack {
     // Raw bucket where files are moved, once they pass the data quality check
     // TODO add a lifecycle policy to archive files to Glacier
     // TODO add a default lock on the objects (WORM)
-    this.cdlRawBucket = new s3.Bucket(this, 'cdlRawBucket', {
-      bucketName: PhysicalName.GENERATE_IF_NEEDED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    this.cdlRawBucket = new CdlS3(this, 'cdlRawBucket', {
+      bucketName: PhysicalName.GENERATE_IF_NEEDED
     })
 
     // Error bucket where files are moved if they don't pass the data quality check
     // Once manually processed, the files are manually removed from the bucket
-    this.cdlErrorBucket = new s3.Bucket(this, 'cdlErrorBucket', {
-      bucketName: PhysicalName.GENERATE_IF_NEEDED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    this.cdlErrorBucket = new CdlS3(this, 'cdlErrorBucket', {
+      bucketName: PhysicalName.GENERATE_IF_NEEDED
     })
 
     // Transformed bucket where files are moved after they are processed (format = jsonl)
     // TODO add a lifecycle policy to archive files to Glacier
     // TODO add a default lock on the objects (WORM)
-    this.cdlTransformedBucket = new s3.Bucket(this, 'cdlTransformedBucket', {
-      bucketName: PhysicalName.GENERATE_IF_NEEDED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    this.cdlTransformedBucket = new CdlS3(this, 'cdlTransformedBucket', {
+      bucketName: PhysicalName.GENERATE_IF_NEEDED
     })
 
     // Enriched bucket where files are moved after they are enriched with calculated emissions (format = jsonl)
     // A compacting job compacts jsonl files into parquet files every day
-    this.cdlEnrichedBucket = new s3.Bucket(this, 'cdlEnrichedBucket', {
-      bucketName: PhysicalName.GENERATE_IF_NEEDED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    this.cdlEnrichedBucket = new CdlS3(this, 'cdlEnrichedBucket', {
+      bucketName: PhysicalName.GENERATE_IF_NEEDED
     })
 
     // Bucket used to store unprocessed data lineage events
     // TODO add a lifecycle policy to archive files to Glacier
-    this.cdlDataLineageBucket = new s3.Bucket(this, 'cdlDataLineageBucket', {
-      bucketName: PhysicalName.GENERATE_IF_NEEDED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    this.cdlDataLineageBucket = new CdlS3(this, 'cdlDataLineageBucket', {
+      bucketName: PhysicalName.GENERATE_IF_NEEDED
     })
 
     // Glue Metadata Catalog Database for enriched data
@@ -72,25 +58,25 @@ export class SharedResourcesStack extends Stack {
     })
 
     new CfnOutput(this, 'CDLEnrichedDataBucket', {
-      value: this.cdlEnrichedBucket.bucketName,
+      value: this.cdlEnrichedBucket.s3Bucket.bucketName,
       description: 'Enriched data bucket with outputs from calculator service',
       exportName: 'CDLEnrichedDataBucket',
     });
 
     new CfnOutput(this, 'CDLEnrichedDataBucketUrl', {
-      value: this.cdlEnrichedBucket.bucketWebsiteUrl,
+      value: this.cdlEnrichedBucket.s3Bucket.bucketWebsiteUrl,
       description: 'Url for enriched data bucket with outputs from calculator service',
       exportName: 'CDLEnrichedDataBucketUrl',
     });
 
     new CfnOutput(this, 'CDLDataLineageBucket', {
-      value: this.cdlDataLineageBucket.bucketName,
+      value: this.cdlDataLineageBucket.s3Bucket.bucketName,
       description: 'Data lineage S3 bucket',
       exportName: 'CDLDataLineageBucket',
     });
 
     new CfnOutput(this, 'CDLDataLineageBucketUrl', {
-      value: this.cdlDataLineageBucket.bucketWebsiteUrl,
+      value: this.cdlDataLineageBucket.s3Bucket.bucketWebsiteUrl,
       description: 'Data lineage S3 bucket URL',
       exportName: 'CDLDataLineageBucketUrl',
     });
