33bf1bec837df3345cf1d81e6037da61b5d8250a
Sam Ward Biddle <sambiddl@amazon.com>
2023-05-12 15:48:46 -0700

    Comment out compaction glue purge if evaluation of date for test: - Check if any side effects of purging data from today bucket after ALL compaction jobs - CHeck if this fix addresses the temporary data duplication issue from manual test

--- lib/stacks/stack-data-compaction/construct-data-compaction-glue/assets/glue-purge-old-calculator-records.py
+++ lib/stacks/stack-data-compaction/construct-data-compaction-glue/assets/glue-purge-old-calculator-records.py
@@ -1,25 +1,24 @@
 import boto3
-from datetime import datetime, date, time 
+from datetime import datetime, date, time
 import sys
 from awsglue.utils import getResolvedOptions
 
 args = getResolvedOptions(sys.argv, ["ENRICHED_BUCKET_NAME"])
 
-today_beginning = datetime.combine(date.today(), time()) 
+today_beginning = datetime.combine(date.today(), time())
 print(today_beginning)
 
 
 try:
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource("s3")
     bucket = s3.Bucket(args["ENRICHED_BUCKET_NAME"])
-    for file in bucket.objects.filter(Prefix='today/'):
-         #compare dates 
-         if (file.last_modified).replace(tzinfo = None) < today_beginning:
-            #print results
-            print('File Name: %s ---- Date: %s' % (file.key,file.last_modified))
-            s3.Object(args["ENRICHED_BUCKET_NAME"], file.key).delete()
+    for file in bucket.objects.filter(Prefix="today/"):
+        # compare dates
+        # commenting out if statement to test effect of removing all today files each time compaction is run
+        # if (file.last_modified).replace(tzinfo = None) < today_beginning:
+        # print results
+        print("File Name: %s ---- Date: %s" % (file.key, file.last_modified))
+        s3.Object(args["ENRICHED_BUCKET_NAME"], file.key).delete()
 
 except Exception as e:
-  print(e)
-  
-  
+    print(e)
