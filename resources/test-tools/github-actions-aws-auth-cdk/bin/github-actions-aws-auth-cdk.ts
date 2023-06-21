c779d7b4473b26eecca310ca0b3bf9cf3726b677
Sam Ward Biddle <sambiddl@amazon.com>
2023-06-21 10:49:31 -0400

    Update global CDK version.

--- resources/test-tools/github-actions-aws-auth-cdk/bin/github-actions-aws-auth-cdk.ts
+++ resources/test-tools/github-actions-aws-auth-cdk/bin/github-actions-aws-auth-cdk.ts
@@ -5,7 +5,13 @@ import { GithubActionsAwsAuthCdkStack } from '../lib/github-actions-aws-auth-cdk
 
 const app = new cdk.App()
 
+const appEnv = {
+  region: app.node.tryGetContext('awsRegion') ? app.node.tryGetContext('awsRegion') : process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+}
+
 new GithubActionsAwsAuthCdkStack(app, 'GithubActionsAwsAuthCdkStack', {
+  env: appEnv,
   repositoryConfig: [
     {
       owner: 'aws-solutions-library',
